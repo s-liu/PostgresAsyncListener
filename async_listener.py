a26b5f6174019ce77f2a86362e72ffd38cf25571
Xiaochen Liu <xiaochen@benchling.com>
2016-04-01 13:35:28 -0700

    first commit

---
+++ async_listener.py
@@ -0,0 +1,53 @@
+import sys
+
+dbname = 'mytestdb'
+host = 'localhost'
+user = 'xiaochen'
+password = ''
+
+dsn = 'dbname=%s host=%s user=%s password=%s' % (dbname, host, user, password)
+
+class AsyncListner():
+
+    def __init__(self, dsn):
+        import psycopg2
+        self.conn = psycopg2.connect(dsn)
+        self.conn.set_isolation_level(0)
+        self.curs = self.conn.cursor()
+        self._listening = False
+
+    def _listen(self):
+        print 'start listening...'
+        from select import select
+        while True:
+            if select([self.conn],[],[],60) != ([],[],[]):
+                self.conn.poll()
+                while self.curs.connection.notifies:
+                    notify = self.curs.connection.notifies.pop()
+                    self.gotNotify(notify)
+
+    def addNotify(self, channel):
+        sql = "LISTEN %s" % channel 
+        self.curs.execute(sql)
+
+    def removeNotify(self, channel):
+        sql = "UNLISTEN %s" % channel 
+        self.curs.execute(sql)
+
+    def run(self):
+        self._listen()
+
+    def gotNotify(self, notify):
+        print 'Gotcha!'
+        if notify.payload:
+            print notify.payload
+
+if __name__ == '__main__':
+    channels = sys.argv[1:]
+    print 'initializing async listener'
+    listener = AsyncListner(dsn)
+    for channel in channels:
+        print 'subscribing to', channel
+        listener.addNotify(channel)
+    print 'done subscribing!'
+    listener.run()
---
+++ init_test_db.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+dbname="mytestdb"
+channel='channel'
+
+dropdb $dbname
+createdb $dbname
+psql $dbname << EOF
+
+CREATE TABLE test_table_1 (
+	msg text
+);
+CREATE TABLE test_table_2 (
+	id int,
+	msg text
+);
+
+CREATE FUNCTION notify_trigger() RETURNS trigger AS \$notify_trigger\$
+BEGIN
+	PERFORM pg_notify('$channel', CAST(NEW AS text));
+	return NEW;
+END;
+\$notify_trigger\$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE OR DELETE ON test_table_1
+	FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
+CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE OR DELETE ON test_table_2
+	FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
+EOF
