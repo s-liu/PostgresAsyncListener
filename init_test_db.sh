be8a35c6190a45dad86731af0de64e8a8e71f22b
s-liu <xcliu616@gmail.com>
2016-04-01 14:17:49 -0700

    convert to json

--- init_test_db.sh
+++ init_test_db.sh
@@ -15,8 +15,15 @@ CREATE TABLE test_table_2 (
 );
 
 CREATE FUNCTION notify_trigger() RETURNS trigger AS \$notify_trigger\$
+DECLARE
+	row RECORD;
 BEGIN
-	PERFORM pg_notify('$channel', CAST(NEW AS text));
+	IF TG_OP = 'INSERT' OR TG_OP = 'UPDATE' THEN
+    	row = NEW;
+  	ELSE
+    	row = OLD;
+  	END IF;
+	PERFORM pg_notify('$channel', json_build_object('table', TG_TABLE_NAME, 'id', row.id, 'type', TG_OP, 'row', row_to_json(row))::text);
 	return NEW;
 END;
 \$notify_trigger\$ LANGUAGE plpgsql;
